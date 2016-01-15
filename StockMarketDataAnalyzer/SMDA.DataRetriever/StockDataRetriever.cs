66c7e10b193e708a8e98da87b016b1c2c7255543
zh <izhulien@yahoo.com>
2016-01-15 16:57:30 +0200

    Add methods for downloading market data in CSV format

--- StockMarketDataAnalyzer/SMDA.DataRetriever/StockDataRetriever.cs
+++ StockMarketDataAnalyzer/SMDA.DataRetriever/StockDataRetriever.cs
@@ -38,5 +38,20 @@
             this.webClient = new WebClient();
             Directory.CreateDirectory(outputDirectoryPath);
         }
+
+        private void DownloadStockData(string companyName)
+        {
+            var today = DateTime.Now;
+
+            this.DownloadStockData(companyName, 1, 1, 2000, today.Day, today.Month, today.Year);
+        }
+
+        private void DownloadStockData(string companyName, int startDay, int startMonth, int startYear, int endDay, int endMonth, int endYear)
+        {
+            string URL = string.Format("http://real-chart.finance.yahoo.com/table.csv?s={0}&d={1}&e={2}&f={3}&g=d&a={4}&b={5}&c={6}&ignore=.csv", companyName, endMonth - 1, endDay, endYear, startMonth - 1, startDay, startYear);
+
+
+            this.webClient.DownloadFile(URL, this.outputDirectoryPath + companyName + ".txt");
+        }
     }
 }
